acc3894412ecaaab7b75b7fd195c3b12a0180f5f
Eason0210 <aqua0210@163.com>
2021-09-12 22:16:58 +0800

    update aliases.

--- config/home/aliases.nix
+++ config/home/aliases.nix
@@ -1,19 +1,22 @@
 {
-  ls = "ls --color=auto -F";
-
-  # u/uu/uuu/... {{{
+  # ls = "ls --color=auto -F";
+  la = "ls -a";
+  
+  # u/uu/uuu/...
   u = "cd ..";
   uu = "cd ../..";
   uuu = "cd ../../..";
   uuuu = "cd ../../../..";
   uuuuu = "cd ../../../../..";
   uuuuuu = "cd ../../../../../..";
-  # }}}
-  # c$n[h]/... {{{
-  # Git {{{
 
+  # Git 
   gb = "git branch";
-
   gtl = "git tag -l";
   ga = "git add";
+
+ # Proxt Setting
+ proxy = "export all_proxy=socks5://127.0.0.1:1080";
+ unproxy = "unset all_proxy";
+
 }
