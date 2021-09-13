fd509d60d9d99d4f93baa37f27803252d07d787e
Eason0210 <aqua0210@163.com>
2021-09-13 23:03:48 +0800

    adjust the structure of home.nix

--- config/home.nix
+++ config/home.nix
@@ -12,13 +12,10 @@ in {
   xdg.dataHome = "/Users/aqua0210/${relativeXDGDataPath}";
   xdg.cacheHome = "/Users/aqua0210/${relativeXDGCachePath}";
 
-  home.packages = callPackage ./packages.nix { };
+  home = {
+    packages = callPackage ./packages.nix { };
 
-  # Let Home Manager install and manage itself.
-  # programs.home-manager.enable = true;
-  programs.bat.enable = true;
-  programs.broot.enable = true;
-  programs.gpg.enable = true;
+  };
 
   # home.file.".gnupg/gpg-agent.conf".text = ''
   # '' + (if pkgs.stdenv.isDarwin then ''
@@ -35,105 +32,113 @@ in {
 
   # home.file.".hammerspoon".source = /b/etc/hammerspoon;
 
-  programs.ssh = {
-    enable = true;
-    # extraConfig = ''
-    #   Include ~/.spin/ssh/include
-    # '';
-  };
+  programs = {
+    # Let Home Manager install and manage itself.
+    # home-manager.enable = true;
+    bat.enable = true;
+    broot.enable = true;
+    gpg.enable = true;
 
-  programs.git = {
-    enable = true;
-    userName = "Eason Huang";
-    userEmail = "aqua0210@163.com";
-    extraConfig = {
-      hub.protocol = "https";
-      github.user = "Eason0210";
-      color.ui = true;
-      pull.rebase = true;
-      merge.conflictstyle = "diff3";
-      credential.helper = "osxkeychain";
-      diff.algorithm = "patience";
-      protocol.version = "2";
-      core.commitGraph = true;
-      gc.writeCommitGraph = true;
+    git = {
+      enable = true;
+      userName = "Eason Huang";
+      userEmail = "aqua0210@163.com";
+      extraConfig = {
+        hub.protocol = "https";
+        github.user = "Eason0210";
+        color.ui = true;
+        pull.rebase = true;
+        merge.conflictstyle = "diff3";
+        credential.helper = "osxkeychain";
+        diff.algorithm = "patience";
+        protocol.version = "2";
+        core.commitGraph = true;
+        gc.writeCommitGraph = true;
+      };
     };
-  };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    history = {
-      path = "${relativeXDGDataPath}/zsh/.zsh_history";
-      size = 50000;
-      save = 50000;
+    ssh = {
+      enable = true;
+      # extraConfig = ''
+      #   Include ~/.spin/ssh/include
+      # '';
     };
-    shellAliases = import ./home/aliases.nix;
-    defaultKeymap = "emacs";
-    # initExtraBeforeCompInit = ''
-    #   eval $(${pkgs.coreutils}/bin/dircolors -b ${./home/LS_COLORS})
-    #   ${builtins.readFile ./home/pre-compinit.zsh}
-    # '';
-    # initExtra = builtins.readFile ./home/post-compinit.zsh;
 
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          rev = "v0.6.3";
-          sha256 = "1h8h2mz9wpjpymgl2p7pc146c1jgb3dggpvzwm9ln3in336wl95c";
-        };
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "be3882aeb054d01f6667facc31522e82f00b5e94";
-          sha256 = "0w8x5ilpwx90s2s2y56vbzq92ircmrf0l5x8hz4g1nx3qzawv6af";
-        };
-      }
-    ];
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      history = {
+        path = "${relativeXDGDataPath}/zsh/.zsh_history";
+        size = 50000;
+        save = 50000;
+      };
+      shellAliases = import ./home/aliases.nix;
+      defaultKeymap = "emacs";
+      # initExtraBeforeCompInit = ''
+      #   eval $(${pkgs.coreutils}/bin/dircolors -b ${./home/LS_COLORS})
+      #   ${builtins.readFile ./home/pre-compinit.zsh}
+      # '';
+      # initExtra = builtins.readFile ./home/post-compinit.zsh;
 
-    sessionVariables = rec {
-      # NVIM_TUI_ENABLE_TRUE_COLOR = "1";
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.6.3";
+            sha256 = "1h8h2mz9wpjpymgl2p7pc146c1jgb3dggpvzwm9ln3in336wl95c";
+          };
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "be3882aeb054d01f6667facc31522e82f00b5e94";
+            sha256 = "0w8x5ilpwx90s2s2y56vbzq92ircmrf0l5x8hz4g1nx3qzawv6af";
+          };
+        }
+      ];
 
-      HOME_MANAGER_CONFIG = /Users/aqua0210/src/nix/config/home.nix;
+      sessionVariables = rec {
+        # NVIM_TUI_ENABLE_TRUE_COLOR = "1";
 
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=3";
-      DEV_ALLOW_ITERM2_INTEGRATION = "1";
+        HOME_MANAGER_CONFIG = /Users/aqua0210/src/nix/config/home.nix;
 
-      EDITOR = "vim";
-      VISUAL = EDITOR;
-      GIT_EDITOR = EDITOR;
+        ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=3";
+        DEV_ALLOW_ITERM2_INTEGRATION = "1";
 
-      GOPATH = "$HOME";
+        EDITOR = "vim";
+        VISUAL = EDITOR;
+        GIT_EDITOR = EDITOR;
 
-      # PATH = "$HOME/bin:$PATH";
+        GOPATH = "$HOME";
+
+        # PATH = "$HOME/bin:$PATH";
+      };
+      # envExtra
+      # profileExtra
+      # loginExtra
+      # logoutExtra
+      # localVariables
     };
-    # envExtra
-    # profileExtra
-    # loginExtra
-    # logoutExtra
-    # localVariables
-  };
 
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-    # extraConfig = builtins.readFile ./home/extraConfig.vim;
+    neovim = {
+      enable = true;
+      vimAlias = true;
+      # extraConfig = builtins.readFile ./home/extraConfig.vim;
 
-    plugins = with pkgs.vimPlugins; [
-      # Syntax / Language Support ##########################
-      vim-nix
-      # UI #################################################
-      gruvbox # colorscheme
-      vim-gitgutter # status in gutter
-      # vim-devicons
-      vim-airline
-    ];
+      plugins = with pkgs.vimPlugins; [
+        # Syntax / Language Support ##########################
+        vim-nix
+        # UI #################################################
+        gruvbox # colorscheme
+        vim-gitgutter # status in gutter
+        # vim-devicons
+        vim-airline
+      ];
+    };
   };
 }
